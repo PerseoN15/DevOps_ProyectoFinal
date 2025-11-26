777e47f137b11ca595beb578e1644f996d082b7f
PerseoN15 <andres.nc@tecjerez.edu.mx>
2025-11-26 12:13:42 -0600

    modificacion de elementos del login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-// frontend/src/App.jsx
-import LoginPage from "./pages/LoginPage";
+/* eslint-disable react/react-in-jsx-scope */
+import React from "react";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
-      <LoginPage />
+      <Login />
     </div>
   );
 }
